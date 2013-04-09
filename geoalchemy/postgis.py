fe5a5bd75d6f91f34f0f0e9a997526d91facb495
schminitz <gillian@affinitic.be>
2013-04-09 12:14:42 +0200

    pep8 fixes ##

--- geoalchemy/postgis.py
+++ geoalchemy/postgis.py
@@ -5,6 +5,7 @@ from geoalchemy.base import SpatialComparator, PersistentSpatialElement, \
 from geoalchemy.dialect import SpatialDialect
 from geoalchemy.functions import functions, BaseFunction
 
+
 class PGComparator(SpatialComparator):
     """Comparator class used for PostGIS
     """
@@ -67,60 +68,61 @@ class pg_functions(functions):
                     func.ST_Expand(geom1, distance).op('&&')(geom2),
                     func.ST_Distance(geom1, geom2) <= distance)
 
+
 class PGSpatialDialect(SpatialDialect):
     """Implementation of SpatialDialect for PostGIS."""
 
     __functions = {
-                   WKTSpatialElement: 'ST_GeomFromText',
-                   WKBSpatialElement: 'ST_GeomFromWKB',
-                   functions.wkt: 'ST_AsText',
-                   functions.wkb: 'ST_AsBinary',
-                   functions.dimension : 'ST_Dimension',
-                   functions.srid : 'ST_SRID',
-                   functions.geometry_type : 'ST_GeometryType',
-                   functions.is_valid : 'ST_IsValid',
-                   functions.is_empty : 'ST_IsEmpty',
-                   functions.is_simple : 'ST_IsSimple',
-                   functions.is_closed : 'ST_IsClosed',
-                   functions.is_ring : 'ST_IsRing',
-                   functions.num_points : 'ST_NumPoints',
-                   functions.point_n : 'ST_PointN',
-                   functions.length : 'ST_Length',
-                   functions.area : 'ST_Area',
-                   functions.x : 'ST_X',
-                   functions.y : 'ST_Y',
-                   functions.centroid : 'ST_Centroid',
-                   functions.boundary : 'ST_Boundary',
-                   functions.buffer : 'ST_Buffer',
-                   functions.convex_hull : 'ST_ConvexHull',
-                   functions.envelope : 'ST_Envelope',
-                   functions.start_point : 'ST_StartPoint',
-                   functions.end_point : 'ST_EndPoint',
-                   functions.transform : 'ST_Transform',
-                   functions.equals : 'ST_Equals',
-                   functions.distance : 'ST_Distance',
-                   functions.within_distance : 'ST_DWithin',
-                   functions.disjoint : 'ST_Disjoint',
-                   functions.intersects : 'ST_Intersects',
-                   functions.touches : 'ST_Touches',
-                   functions.crosses : 'ST_Crosses',
-                   functions.within : 'ST_Within',
-                   functions.overlaps : 'ST_Overlaps',
-                   functions.gcontains : 'ST_Contains',
-                   functions.covers : 'ST_Covers',
-                   functions.covered_by : 'ST_CoveredBy',
-                   functions.intersection : 'ST_Intersection',
-                   functions.union : 'ST_Union',
-                   functions.collect : 'ST_Collect',
-                   functions.extent : 'ST_Extent',
-                   pg_functions.svg : 'ST_AsSVG',
-                   pg_functions.kml : 'ST_AsKML',
-                   pg_functions.gml : 'ST_AsGML',
-                   pg_functions.geojson : 'ST_AsGeoJSON',
-                   pg_functions.expand : 'ST_Expand',
-                   pg_functions.distance_sphere : 'ST_Distance_Sphere',
-                   functions._within_distance : pg_functions._within_distance
-                  }
+        WKTSpatialElement: 'ST_GeomFromText',
+        WKBSpatialElement: 'ST_GeomFromWKB',
+        functions.wkt: 'ST_AsText',
+        functions.wkb: 'ST_AsBinary',
+        functions.dimension: 'ST_Dimension',
+        functions.srid: 'ST_SRID',
+        functions.geometry_type: 'ST_GeometryType',
+        functions.is_valid: 'ST_IsValid',
+        functions.is_empty: 'ST_IsEmpty',
+        functions.is_simple: 'ST_IsSimple',
+        functions.is_closed: 'ST_IsClosed',
+        functions.is_ring: 'ST_IsRing',
+        functions.num_points: 'ST_NumPoints',
+        functions.point_n: 'ST_PointN',
+        functions.length: 'ST_Length',
+        functions.area: 'ST_Area',
+        functions.x: 'ST_X',
+        functions.y: 'ST_Y',
+        functions.centroid: 'ST_Centroid',
+        functions.boundary: 'ST_Boundary',
+        functions.buffer: 'ST_Buffer',
+        functions.convex_hull: 'ST_ConvexHull',
+        functions.envelope: 'ST_Envelope',
+        functions.start_point: 'ST_StartPoint',
+        functions.end_point: 'ST_EndPoint',
+        functions.transform: 'ST_Transform',
+        functions.equals: 'ST_Equals',
+        functions.distance: 'ST_Distance',
+        functions.within_distance: 'ST_DWithin',
+        functions.disjoint: 'ST_Disjoint',
+        functions.intersects: 'ST_Intersects',
+        functions.touches: 'ST_Touches',
+        functions.crosses: 'ST_Crosses',
+        functions.within: 'ST_Within',
+        functions.overlaps: 'ST_Overlaps',
+        functions.gcontains: 'ST_Contains',
+        functions.covers: 'ST_Covers',
+        functions.covered_by: 'ST_CoveredBy',
+        functions.intersection: 'ST_Intersection',
+        functions.union: 'ST_Union',
+        functions.collect: 'ST_Collect',
+        functions.extent: 'ST_Extent',
+        pg_functions.svg: 'ST_AsSVG',
+        pg_functions.kml: 'ST_AsKML',
+        pg_functions.gml: 'ST_AsGML',
+        pg_functions.geojson: 'ST_AsGeoJSON',
+        pg_functions.expand: 'ST_Expand',
+        pg_functions.distance_sphere: 'ST_Distance_Sphere',
+        functions._within_distance: pg_functions._within_distance
+    }
 
     def _get_function_mapping(self):
         return PGSpatialDialect.__functions
@@ -143,9 +145,8 @@ class PGSpatialDialect(SpatialDialect):
                                                     column.type.dimension)]).execution_options(autocommit=True))
         if column.type.spatial_index:
             bind.execute("CREATE INDEX \"idx_%s_%s\" ON \"%s\".\"%s\" USING GIST (%s)" %
-                            (table.name, column.name, (table.schema or 'public'), table.name, column.name))
+                         (table.name, column.name, (table.schema or 'public'), table.name, column.name))
 
         if not column.nullable:
             bind.execute("ALTER TABLE \"%s\".\"%s\" ALTER COLUMN \"%s\" SET not null" %
-                            ((table.schema or 'public'), table.name, column.name))
-
+                         ((table.schema or 'public'), table.name, column.name))
